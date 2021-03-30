eb4ad3070298304eb9d68fcdde48e539184943af
Timothy Do <tydo83@gmail.com>
2021-03-30 15:05:11 -0400

    added signup and login

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -1,5 +1,51 @@
 const User = require('../Model/User')
+const bcrypt = require('bcryptjs')
 
 module.exports = {
-    
+    signUp: async(req, res) => {
+        try {
+            let salted = await bcrypt.genSalt(10);
+            let hashedPassword = await bcrypt.hash(req.body.password, salted);
+
+            let createdUser = await new User({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: hashedPassword,
+            })
+            let savedUser = await createdUser.save();
+
+            res.json({
+                data: savedUser,
+            })
+        } catch(e) {
+            res.status(500).json({
+                message: e,
+            })
+        }
+    },
+    login: async(req, res) => {
+        try {
+            let foundUser = await User.findOne({ email: req.body.email });
+            if(!foundUser) {
+                throw { message: "Email is not registered, please sign up" };
+            }
+            let comparedPassword = await bcrypt.compare(
+                req.body.password,
+                foundUser.password,
+            );
+            if(!comparedPassword) {
+                throw { message: "Please check your email and password"}
+            } else {
+                res.json({
+                    message: "success"
+                })  
+            }
+
+        } catch(e) {
+            res.status(500).json({
+                message: e,
+            })
+        }
+    }
 }
--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var { signUp, login } = require('./controller/userController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/sign-up', signUp);
+router.post('/login', login)
 
 module.exports = router;
