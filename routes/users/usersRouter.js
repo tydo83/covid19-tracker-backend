95ddea95b505c728925ce729bce7bdc46d635f93
Timothy Do <tydo83@gmail.com>
2021-03-31 14:29:07 -0400

    added authChecker

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var { checkIfEmptyMiddleware, checkForSymbolMiddleware, checkLoginIsEmpty } = require('../lib/validator')
+var { checkIsUserHaveValidJwtToken } = require('../lib/authChecker')
+
+
 var { signUp, login } = require('./controller/userController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/sign-up', signUp);
-router.post('/login', login)
+router.post('/sign-up', checkIfEmptyMiddleware, checkForSymbolMiddleware, signUp);
+router.post('/login', checkLoginIsEmpty, login)
 
 module.exports = router;
